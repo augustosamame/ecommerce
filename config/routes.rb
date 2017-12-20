d93b5cb49097af925e0886fcaa0c55401e6f5e6e
Augusto Samame <augustosamame@gmail.com>
2017-12-20 13:25:17 -0500

    added js and css for backoffice

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Ecommerce::Engine.routes.draw do
 
   namespace :backoffice do
+    get 'dashboard'
+    resources :users
+    resources :controls
     resources :products
     resources :product_variants
     resources :product_skus
     resources :product_sku_properties
+    resources :orders
   end
-  
+
   root to: 'store#main'
 
 end
