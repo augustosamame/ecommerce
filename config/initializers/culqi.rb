63aa0cac0d31bac76058e669a052c470cedc1fde
Augusto Samame <augustosamame@gmail.com>
2018-07-31 22:29:55 -0500

    check if payment_method table exists before initiliazing Culqi

--- config/initializers/culqi.rb
+++ config/initializers/culqi.rb
@@ -1,3 +1,4 @@
-#raise "Must set Culqi public_key and secret_key as Ecommerce::PaymentMethod records"
-Culqi.public_key = Ecommerce::PaymentMethod.find_by(name: "Card", processor: "Culqi").try(:key)
-Culqi.secret_key = Ecommerce::PaymentMethod.find_by(name: "Card", processor: "Culqi").try(:secret)
+if ActiveRecord::Base.connection.table_exists? 'ecommerce_payment_methods'
+  Culqi.public_key = Ecommerce::PaymentMethod.find_by!(name: "Card", processor: "Culqi").try(:key)
+  Culqi.secret_key = Ecommerce::PaymentMethod.find_by!(name: "Card", processor: "Culqi").try(:secret)
+end
