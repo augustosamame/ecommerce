c20942ad6f69c9ebf9c5b8a0c097eb5861dff33d
Augusto Samamé <augustosamame@gmail.com>
2019-03-07 10:45:21 -0500

    fix bulk emailer worker

--- app/workers/send_campaign_email_worker.rb
+++ app/workers/send_campaign_email_worker.rb
@@ -3,7 +3,7 @@ class SendCampaignEmailWorker
 
   def perform(user_id, coupon_id, campaign_id)
     user = User.find(user_id)
-    coupon = Ecommerce::Coupon.find_by(coupon_id)
+    coupon = Ecommerce::Coupon.find_by(id: coupon_id)
     campaign = Ecommerce::Campaign.find(campaign_id)
     UserMailer.send_campaign_email(user, coupon, campaign).deliver! #unless Rails.env == "development"
   end
