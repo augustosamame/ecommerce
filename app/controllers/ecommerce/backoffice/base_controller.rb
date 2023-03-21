84b578cb9cf0995c7ba9d2d9f4b87af617d39559
Augusto Samame <augustosamame@gmail.com>
2023-03-21 17:42:50 -0500

    implement redis search indexes

--- app/controllers/ecommerce/backoffice/base_controller.rb
+++ app/controllers/ecommerce/backoffice/base_controller.rb
@@ -14,11 +14,40 @@ module Ecommerce
     end
 
     def set_dashboard_searches
+      @redis = Redis.new(host: ENV['REDIS_ENDPOINT'])
+      if @redis.get("all_users")
+        @users_array = JSON.parse(@redis.get("all_users"))
+      else
+        set_user_dashboard_searches
+      end
+      if @redis.get("all_orders")
+        @orders_array = JSON.parse(@redis.get("all_orders"))
+      else
+        set_order_dashboard_searches
+      end
+      if @redis.get("all_products")
+        @products_array = JSON.parse(@redis.get("all_products"))
+        @category_products_array = JSON.parse(@redis.get("all_category_products"))
+      else
+        set_product_dashboard_searches
+      end
+    end
+
+    def set_user_dashboard_searches
       @users_array = User.all.order(id: :desc).map{|f| {label: f.name, id: f.id}}
-      #@products_array = Product.all.map{|f| {label: "#{f.name} - #{f.price_cents.to_soles}", value: f.name, id: f.id}}
+      @redis.setex("all_users", 600, @users_array.to_json)
+    end
+
+    def set_order_dashboard_searches
+      @orders_array = Order.all.includes(:user).order(id: :desc).map{|f| {label: "Order ##{f.id} - #{f.user.name}", value: f.user.name, id: f.id}}
+      @redis.setex("all_orders", 600, @orders_array.to_json)
+    end
+
+    def set_product_dashboard_searches
       @products_array = Product.all.map{|f| {label: "#{f.name}", value: f.name, id: f.id}}
       @category_products_array = Product.all.map{|f| {label: "#{f.name} - #{f.category_list.to_csv}", value: f.name, id: f.id}}
-      @orders_array = Order.all.includes(:user).order(id: :desc).map{|f| {label: "Order ##{f.id} - #{f.user.name}", value: f.user.name, id: f.id}}
+      @redis.setex("all_products", 600, @products_array.to_json)
+      @redis.setex("all_category_products", 600, @category_products_array.to_json)
     end
 
     def set_default_locale
