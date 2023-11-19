53039730f5110e1dbb340aac786f3aec23fcc6fa
Augusto Samame <augustosamame@gmail.com>
2023-11-19 09:58:59 -0500

    fix mabile shopping cart and readd shake effect

--- app/assets/javascripts/ecommerce/expatshop/custom_expatshop.js
+++ app/assets/javascripts/ecommerce/expatshop/custom_expatshop.js
@@ -1,3 +1,105 @@
+function fly_to_cart() {
+  //console.log ("function fly_to_cart fired!");
+  $('#cart-line').shake({
+    interval: 100,
+    distance: 20,
+    times: 5
+  });
+};
+
+function show_stock_alert(lang) {
+  if (lang == 'es-PE') {
+    Swal({
+      type: 'info',
+      title: 'Le avisaremos cuando el producto esté nuevamente en Stock',
+      text: ""
+    });
+  } else {
+    Swal({
+      type: 'info',
+      title: 'We will alert you when product is back in stock',
+      text: ""
+    });
+  }
+};
+
+function show_stock_alert_signed_out(lang) {
+  if (lang == 'es-PE') {
+    Swal({
+      type: 'error',
+      title: 'Debe iniciar sesión o registrarse para crear alertas de stock.',
+      text: ""
+    });
+  } else {
+    Swal({
+      type: 'error',
+      title: 'You must sign in or register to generate a stock alert.',
+      text: ""
+    });
+  }
+};
+
+function add_to_cart_no_stock() {
+  //console.log ("function fly_to_cart fired!");
+  //$('#shopping-cart').shake({
+  //    interval: 100,
+  //    distance: 20,
+  //    times: 5
+  //});
+  alert("Product is Out of Stock");
+};
+
+$.fn.shake = function (settings) {
+  if (typeof settings.interval == 'undefined') {
+    settings.interval = 100;
+  }
+
+  if (typeof settings.distance == 'undefined') {
+    settings.distance = 10;
+  }
+
+  if (typeof settings.times == 'undefined') {
+    settings.times = 4;
+  }
+
+  if (typeof settings.complete == 'undefined') {
+    settings.complete = function () { };
+  }
+
+  $(this).css('position', 'relative');
+
+  for (var iter = 0; iter < (settings.times + 1); iter++) {
+    $(this).animate({ left: ((iter % 2 == 0 ? settings.distance : settings.distance * -1)) }, settings.interval);
+  }
+
+  $(this).animate({ left: 0 }, settings.interval, settings.complete);
+};
+$.fn.bounce = function (settings) {
+  if (typeof settings.interval == 'undefined') {
+    settings.interval = 100;
+  }
+
+  if (typeof settings.distance == 'undefined') {
+    settings.distance = 10;
+  }
+
+  if (typeof settings.times == 'undefined') {
+    settings.times = 4;
+  }
+
+  if (typeof settings.complete == 'undefined') {
+    settings.complete = function () { };
+  }
+
+  $(this).css('position', 'relative');
+
+  for (var iter = 0; iter < (settings.times + 1); iter++) {
+    $(this).animate({ top: ((iter % 2 == 0 ? settings.distance : settings.distance * -1)) }, settings.interval);
+  }
+
+  $(this).animate({ top: 0 }, settings.interval, settings.complete);
+};
+
 // brand slider
 $(function () {
   $(".brand-slider").slick({
--- lib/ecommerce/version.rb
+++ lib/ecommerce/version.rb
@@ -1,3 +1,3 @@
 module Ecommerce
-  VERSION = '0.8.0.8'
+  VERSION = '0.8.0.9'
 end
