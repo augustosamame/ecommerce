90fed6b5da66a42d42f81212fad364d2d1ee2fe2
Augusto Samame <augustosamame@gmail.com>
2023-11-28 22:46:22 -0500

    check if cart exists before calculating combos

--- app/controllers/ecommerce/application_controller.rb
+++ app/controllers/ecommerce/application_controller.rb
@@ -24,100 +24,101 @@ module Ecommerce
       @friendly_combo_discount_array = []
 
       my_cart = Cart.where(id: session[:cart_id], status: "active").order(:id).last
-      cart_items = my_cart.cart_items
-      cart_items_product_ids = cart_items.pluck(:product_id).flatten.uniq
-      combo_discounts_product_ids = Ecommerce::ComboDiscount.pluck(:product_id_1, :product_id_2).flatten.compact.uniq
-      common_product_ids = (cart_items_product_ids & combo_discounts_product_ids)
+      if my_cart
+        cart_items = my_cart.cart_items
+        cart_items_product_ids = cart_items.pluck(:product_id).flatten.uniq
+        combo_discounts_product_ids = Ecommerce::ComboDiscount.pluck(:product_id_1, :product_id_2).flatten.compact.uniq
+        common_product_ids = (cart_items_product_ids & combo_discounts_product_ids)
 
-      if common_product_ids.present?
+        if common_product_ids.present?
 
-        #consolidate_cart_items
-        consolidated_cart_items = {}
-        cart_items.each do |item|
-          if consolidated_cart_items.key?(item[:product_id])
-            # If it exists, add the cart_product_qty to the existing value
-            consolidated_cart_items[item[:product_id]][:quantity] += item[:quantity]
-          else
-            # If it doesn't exist, create a new entry in the consolidated_cart_items hash
-            consolidated_cart_items[item[:product_id]] = item
-          end
-        end
-        consolidated_cart_items = consolidated_cart_items.values
-
-        # Print the consolidated cart items
-        consolidated_cart_items.each do |item|
-          puts "Product ID: #{item[:product_id]}, Quantity: #{item[:quantity]}"
-        end
-
-        global_discount = 0
-        global_discount_usd = 0
-        Ecommerce::ComboDiscount.active.each do |combo_discount|
-
-          cart_item_that_matches_combo_discount_product_1 = nil
-          cart_item_that_matches_combo_discount_product_2 = nil
-          combo_added = false
-          number_of_combos = 0
-
-          #find_matching_product_1
-          consolidated_cart_items.each do |item|
-            matching_product_1 = combo_discount.product_id_1 == item[:product_id]
-            if matching_product_1 && combo_discount.qty_product_1 <= item[:quantity]
-              number_of_combos = (item[:quantity] / combo_discount.qty_product_1).floor
-              cart_item_that_matches_combo_discount_product_1 = item
-            end
-          end
-          
-          if cart_item_that_matches_combo_discount_product_1
-            if combo_discount.product_id_2.present?
-              #find_matching_product_2
-              consolidated_cart_items.each do |item|
-                matching_product_2 = combo_discount.product_id_2 == item[:product_id]
-                if matching_product_2 && combo_discount.qty_product_2 <= item[:quantity]
-                  new_number_of_combos = (item[:quantity] / combo_discount.qty_product_2).floor
-                  #check that the number of combos for product 2 is less or equal than the number of combos for product 1
-                  if new_number_of_combos <= number_of_combos
-                    number_of_combos = new_number_of_combos
-                  end
-                  cart_item_that_matches_combo_discount_product_2 = item
-                  @combo_discount_array << combo_discount
-                  combo_added = true
-                end
-              end
+          #consolidate_cart_items
+          consolidated_cart_items = {}
+          cart_items.each do |item|
+            if consolidated_cart_items.key?(item[:product_id])
+              # If it exists, add the cart_product_qty to the existing value
+              consolidated_cart_items[item[:product_id]][:quantity] += item[:quantity]
             else
-              @combo_discount_array << combo_discount
-              combo_added = true
+              # If it doesn't exist, create a new entry in the consolidated_cart_items hash
+              consolidated_cart_items[item[:product_id]] = item
             end
           end
+          consolidated_cart_items = consolidated_cart_items.values
 
-          if combo_added
+          # Print the consolidated cart items
+          consolidated_cart_items.each do |item|
+            puts "Product ID: #{item[:product_id]}, Quantity: #{item[:quantity]}"
+          end
 
-            line_discount = 0
-            line_discount_usd = 0
-            discount_prod_1 = 0
-            discount_prod_2 = 0
-            if combo_discount.discount_type == "percentage_discount"
-              product_price = cart_session_price(cart_item_that_matches_combo_discount_product_1.product)
-              discount_prod_1 = combo_discount.qty_product_1 * product_price * (combo_discount.discount_amount.to_f / 100) * number_of_combos.to_f
-              if cart_item_that_matches_combo_discount_product_2.present?
-                product_price = cart_session_price(cart_item_that_matches_combo_discount_product_2.product)
-                discount_prod_2 = combo_discount.qty_product_2 * product_price * combo_discount.discount_amount.to_f / 100 * number_of_combos.to_f
+          global_discount = 0
+          global_discount_usd = 0
+          Ecommerce::ComboDiscount.active.each do |combo_discount|
+
+            cart_item_that_matches_combo_discount_product_1 = nil
+            cart_item_that_matches_combo_discount_product_2 = nil
+            combo_added = false
+            number_of_combos = 0
+
+            #find_matching_product_1
+            consolidated_cart_items.each do |item|
+              matching_product_1 = combo_discount.product_id_1 == item[:product_id]
+              if matching_product_1 && combo_discount.qty_product_1 <= item[:quantity]
+                number_of_combos = (item[:quantity] / combo_discount.qty_product_1).floor
+                cart_item_that_matches_combo_discount_product_1 = item
               end
-              line_discount = discount_prod_1 + discount_prod_2
-              line_discount_usd = session[:currency] == "usd" ? line_discount : (line_discount / @exchange_rate)
-              global_discount_usd += line_discount_usd
-            elsif combo_discount.discount_type == "fixed_discount"
-              line_discount = session[:currency] == "usd" ? (combo_discount.discount_amount * number_of_combos) : (combo_discount.discount_amount * number_of_combos * @exchange_rate)
-              line_discount_usd = combo_discount.discount_amount * number_of_combos
-              global_discount_usd += line_discount_usd
             end
-            @friendly_combo_discount_array << {product_name: session[:locale] == "en-PE" ? combo_discount.description_en : combo_discount.description_es, discount_amount: line_discount}
-            @combo_total_discount_usd = global_discount_usd
+            
+            if cart_item_that_matches_combo_discount_product_1
+              if combo_discount.product_id_2.present?
+                #find_matching_product_2
+                consolidated_cart_items.each do |item|
+                  matching_product_2 = combo_discount.product_id_2 == item[:product_id]
+                  if matching_product_2 && combo_discount.qty_product_2 <= item[:quantity]
+                    new_number_of_combos = (item[:quantity] / combo_discount.qty_product_2).floor
+                    #check that the number of combos for product 2 is less or equal than the number of combos for product 1
+                    if new_number_of_combos <= number_of_combos
+                      number_of_combos = new_number_of_combos
+                    end
+                    cart_item_that_matches_combo_discount_product_2 = item
+                    @combo_discount_array << combo_discount
+                    combo_added = true
+                  end
+                end
+              else
+                @combo_discount_array << combo_discount
+                combo_added = true
+              end
+            end
+
+            if combo_added
+
+              line_discount = 0
+              line_discount_usd = 0
+              discount_prod_1 = 0
+              discount_prod_2 = 0
+              if combo_discount.discount_type == "percentage_discount"
+                product_price = cart_session_price(cart_item_that_matches_combo_discount_product_1.product)
+                discount_prod_1 = combo_discount.qty_product_1 * product_price * (combo_discount.discount_amount.to_f / 100) * number_of_combos.to_f
+                if cart_item_that_matches_combo_discount_product_2.present?
+                  product_price = cart_session_price(cart_item_that_matches_combo_discount_product_2.product)
+                  discount_prod_2 = combo_discount.qty_product_2 * product_price * combo_discount.discount_amount.to_f / 100 * number_of_combos.to_f
+                end
+                line_discount = discount_prod_1 + discount_prod_2
+                line_discount_usd = session[:currency] == "usd" ? line_discount : (line_discount / @exchange_rate)
+                global_discount_usd += line_discount_usd
+              elsif combo_discount.discount_type == "fixed_discount"
+                line_discount = session[:currency] == "usd" ? (combo_discount.discount_amount * number_of_combos) : (combo_discount.discount_amount * number_of_combos * @exchange_rate)
+                line_discount_usd = combo_discount.discount_amount * number_of_combos
+                global_discount_usd += line_discount_usd
+              end
+              @friendly_combo_discount_array << {product_name: session[:locale] == "en-PE" ? combo_discount.description_en : combo_discount.description_es, discount_amount: line_discount}
+              @combo_total_discount_usd = global_discount_usd
+            end
+
           end
 
         end
-
       end
-      #byebug
     end
 
     def add_stretched_to_body_tag
