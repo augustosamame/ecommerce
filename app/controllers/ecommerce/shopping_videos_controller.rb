6e6a08eb10e5cb1e3af48fc32067f841a88c4c96
Augusto Samame <augustosamame@gmail.com>
2024-09-30 17:46:48 -0500

    landing page shopping_videos support category or products

--- app/controllers/ecommerce/shopping_videos_controller.rb
+++ app/controllers/ecommerce/shopping_videos_controller.rb
@@ -18,13 +18,18 @@ module Ecommerce
       shopping_video = Ecommerce::ShoppingVideo.find(params[:id])
       overlays = shopping_video.shopping_video_overlays.active.map do |overlay|
         {
-          product_id: overlay.product.id,
-          product_name: overlay.product.name,
-          product_price: product_session_price(overlay.product, "price"),
-          product_image_url: overlay.product.image_url,
+          product_id: overlay&.product&.id,
+          product_name: overlay&.product&.name,
+          product_price: overlay&.overlay_type == "product" ? product_session_price(overlay&.product, "price") : nil,
+          product_image_url: overlay&.product&.image_url,
+          category_id: overlay&.category&.id,
+          category_name: overlay&.category&.name,
+          category_image_url: overlay&.category&.image_url,
+          overlay_type: overlay&.overlay_type,
           start_time: overlay.start_time,
           end_time: overlay.end_time,
-          add_to_cart_label: I18n.t("ecommerce.shopping_videos.add_to_cart_label")
+          add_to_cart_label: I18n.t("ecommerce.shopping_videos.add_to_cart_label"),
+          view_products_label: I18n.t("ecommerce.shopping_videos.view_products_label")
         }
       end
       render json: overlays
