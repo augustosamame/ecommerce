c92ce8479041a374fad18445c84470567ddcc92e
Augusto Samame <augustosamame@gmail.com>
2024-09-30 09:13:25 -0500

    respect language and currency for overlays and remove scrollbars

--- app/controllers/ecommerce/shopping_videos_controller.rb
+++ app/controllers/ecommerce/shopping_videos_controller.rb
@@ -3,14 +3,16 @@ require_dependency "ecommerce/application_controller"
 module Ecommerce
   class ShoppingVideosController < ApplicationController
     include ActionView::Helpers::NumberHelper
-    
+    include Ecommerce::ApplicationHelper
+
     def overlays
       shopping_video = Ecommerce::ShoppingVideo.find(params[:id])
       overlays = shopping_video.shopping_video_overlays.map do |overlay|
         {
           product_id: overlay.product.id,
           product_name: overlay.product.name,
-          product_price: number_to_currency(overlay.product.price),
+          #product_price: number_to_currency(overlay.product.price),
+          product_price: product_session_price(overlay.product, "price"),
           product_image_url: overlay.product.image_url,
           start_time: overlay.start_time,
           end_time: overlay.end_time
