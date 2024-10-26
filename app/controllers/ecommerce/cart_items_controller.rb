851edd3d4a22b432924c001d18300301a179891f
Augusto Samame <augustosamame@gmail.com>
2024-10-26 10:25:03 -0500

    fix for phantom line in combodiscounts small fix 2

--- app/controllers/ecommerce/cart_items_controller.rb
+++ app/controllers/ecommerce/cart_items_controller.rb
@@ -25,7 +25,7 @@ module Ecommerce
           found_same_product.update(quantity: new_quantity)
           @combo_discount_exists = ComboDiscount.where(status: "active", product_id_1: @product.id, inject_product_two: true).try(:first)
           if @combo_discount_exists.present?
-            @number_of_combos = (@cart_item.quantity / @combo_discount_exists.qty_product_1).floor
+            @number_of_combos = (found_same_product.quantity / @combo_discount_exists.qty_product_1).floor
             if @number_of_combos > 0
               @cart_item_two = CartItem.new(cart_id: @cart.id, product_id: @combo_discount_exists.product_id_2, quantity: @combo_discount_exists.qty_product_2 * @number_of_combos)
               @cart_item_two.save
