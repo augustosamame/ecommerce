c92699082dd37a7b287b3a93162797cbdac2667a
Augusto Samame <augustosamame@gmail.com>
2025-12-20 09:41:05 -0500

    allow delivery picture to be uploaded in all orders

--- app/controllers/ecommerce/backoffice/proof_of_delivery_images_controller.rb
+++ app/controllers/ecommerce/backoffice/proof_of_delivery_images_controller.rb
@@ -12,9 +12,17 @@ module Ecommerce
       @proof_of_delivery_image.user = current_user
 
       if @proof_of_delivery_image.save
-        redirect_to backoffice_order_path(@order), notice: 'Imagen subida correctamente.'
+        if params[:return_to] == 'index'
+          redirect_to backoffice_orders_path, notice: 'Imagen subida correctamente.'
+        else
+          redirect_to backoffice_order_path(@order), notice: 'Imagen subida correctamente.'
+        end
       else
-        redirect_to backoffice_order_path(@order), alert: 'Error al subir la imagen. Por favor intente nuevamente.'
+        if params[:return_to] == 'index'
+          redirect_to backoffice_orders_path, alert: 'Error al subir la imagen. Por favor intente nuevamente.'
+        else
+          redirect_to backoffice_order_path(@order), alert: 'Error al subir la imagen. Por favor intente nuevamente.'
+        end
       end
     end
 
